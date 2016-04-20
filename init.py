889fe292b4f2d58bedf8b7401af04e2acbface88
Martin Wright <martin@bang-on.net>
2016-04-20 17:45:36 +0100

    Made more improvements still a work in progress.

--- init.py
+++ init.py
@@ -2,18 +2,26 @@
 
 # -*- coding: latin-1 -*-
 
-page = "http://www.justiceinspectorates.gov.uk/hmic/publications/strategic-policing-requirement-inspection-force-findings/"
-
 import urllib2
-html_doc = urllib2.urlopen(page)
-
+import sitemap
 from bs4 import BeautifulSoup
-soup = BeautifulSoup(html_doc, 'html.parser')
 
-links = soup.find_all('a')
+urls = sitemap.urls
 
-print page
+for url in urls:
 
-for link in links:
-	if ".pdf" in link.get('href'):
-		print link
+	try: 
+		html_doc = urllib2.urlopen(url)
+
+		soup = BeautifulSoup(html_doc, 'html.parser')
+
+		links = soup.find_all('a')
+
+		for link in links:
+			if link.get('href') and ".pdf" in link.get('href'):
+			
+				href = link.get('href');
+				onclick = link.get('onclick');
+			
+				print '"' + str(url) + '","' + str(href) + '","' + str(onclick) + '"'
+	except: urllib2.HTTPError
